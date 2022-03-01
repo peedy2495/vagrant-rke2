17bf8290abf3cb1c1ddc8aaa9eb8223f095eeecd
Peedy2495 <you@example.com>
2022-03-01 14:53:53 +0100

    added: pulling seaweedfs

--- assets/pull_binaries.sh
+++ assets/pull_binaries.sh
@@ -17,9 +17,11 @@ if [ ! -f $exedir/bin/yq ]; then
 fi
 
 # get rke2
-if [ ! -f $exedir/bin/rke2 ]; then
+if [ ! -f $exedir/bin/rke2-install.sh ]; then
     echo 'Pulling rke2 artifacts ...'
     source assets/gitrepos/shell-toolz/toolz_github.sh
+    OS=$(yq e .env_common.ostype assets/environment.yaml)
+    ARCH=$(yq e .env_common.arch assets/environment.yaml)
     RELEASE=$(yq e .services.rke2.release assets/environment.yaml)
     CNI=$(yq e .services.rke2.cni assets/environment.yaml)
 
@@ -31,4 +33,19 @@ if [ ! -f $exedir/bin/rke2 ]; then
     wget -qO $exedir/bin/rke2.$OS-$ARCH.tar.gz $(GH_GetFileDownloadURL rancher/rke2 rke2.$OS-$ARCH.tar.gz $RELEASE)
     wget -qO $exedir/bin/sha256sum-$ARCH.txt $(GH_GetFileDownloadURL rancher/rke2 sha256sum-$ARCH.txt $RELEASE)
     wget -qO $exedir/bin/rke2-install.sh https://get.rke2.io
+fi
+
+# get SeaweedFS
+if [ ! -f $exedir/bin/weed ]; then
+    echo 'Pulling rke2 artifacts ...'
+    source assets/gitrepos/shell-toolz/toolz_github.sh
+    OS=$(yq e .env_common.ostype assets/environment.yaml)
+    ARCH=$(yq e .env_common.arch assets/environment.yaml)
+    RELEASE=$(yq e .services.seaweedfs.release assets/environment.yaml)
+
+    if [ "$RELEASE" = "latest" ]; then
+        RELEASE=''      #empty = latest
+    fi
+
+    wget -qO $exedir/bin/weed-$OS\_$ARCH.tar.gz $(GH_GetFileDownloadURL chrislusf/seaweedfs $OS\_$ARCH.tar.gz $RELEASE)
 fi
