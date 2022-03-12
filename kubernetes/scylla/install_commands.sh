65a1524c8d325a2fb371418c364a18f8096942e1
peedy2495 <mail.petermark@web.de>
2022-03-12 09:09:58 +0100

    scylla working with monitoring

--- kubernetes/scylla/install_commands.sh
+++ kubernetes/scylla/install_commands.sh
@@ -2,7 +2,7 @@
 
 # create persistant volumes
 mkdir /kube-data-scylla
-kubectl apply -f scylla_pv.yaml
+kubectl apply -f scylla-pv.yaml
 
 # create certificates
 kubectl apply -f create-scylla-certs.yaml
@@ -18,6 +18,7 @@ helm install monitoring prometheus-community/kube-prometheus-stack --values prom
 # Scylla Service Monitor
 kubectl apply -f scylla-service-monitor.yaml
 # Scylla Manager Service Monitor
+kubectl create namespace scylla-manager
 kubectl apply -f scylla-manager-service-monitor.yaml
 # download dashboards
 wget https://github.com/scylladb/scylla-monitoring/archive/scylla-monitoring-3.6.0.tar.gz
@@ -29,12 +30,17 @@ kubectl -n scylla-monitoring create configmap scylla-manager-dashboards --from-f
 kubectl -n scylla-monitoring patch configmap scylla-manager-dashboards  -p '{"metadata":{"labels":{"grafana_dashboard": "1"}}}'
 # port forwarding
 # kubectl -n scylla-monitoring port-forward deployment.apps/monitoring-grafana 3000
+kubectl edit service/monitoring-grafana -n scylla-monitoring
 
+kubectl delete servicemonitor/scylla-service-monitor -n scylla
 helm upgrade --install scylla --namespace scylla scylla/scylla -f cluster-config.yaml
+kubectl exec -it -n scylla -c scylla scylla-we-test-0 -- /bin/sh
+cqlsh
 
 # operator
 helm install scylla-operator scylla/scylla-operator --values operator-config.yaml --namespace scylla-operator
 # cluster
+# edit validatingwebhookconfiguration to include secret name
 helm install scylla scylla/scylla --values cluster-config.yaml --namespace scylla
 # manager
 kubectl apply -f scylla-manager-pv.yaml
